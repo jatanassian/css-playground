700d0a8b627be60856298ef974115e4311d10b49
Julien Atanassian <julien.atanassian@gmail.com>
2021-11-01 21:31:18 -0400

    add content to components

--- components/Box.vue
+++ components/Box.vue
@@ -1,11 +1,34 @@
 <template>
   <section>
-    <div class="box" />
+    <div class="box" :style="style" />
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Box'
+  name: 'Box',
+  props: {
+    transformValues: Object
+  },
+  computed: {
+    style () {
+      return {
+        transform: `
+          perspective(${this.transformValues.perspective}px)
+          rotateX(${this.transformValues.rotateX}deg)
+          rotateY(${this.transformValues.rotateY}deg)
+          rotateZ(${this.transformValues.rotateZ}deg)
+        `
+      }
+    }
+  }
 }
 </script>
+
+<style>
+.box {
+  width: 150px;
+  height: 150px;
+  background: #8d81f3;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
   <main>
     <h1>CSS Playground</h1>
-    <Settings />
-    <Box />
+    <Settings :transform-values="transform" />
+    <Box :transform-values="transform" />
   </main>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'App',
+  data () {
+    return {
+      transform: {
+        perspective: 100,
+        rotateX: 0,
+        rotateY: 0,
+        rotateZ: 0
+      }
+    }
+  },
+  methods: {
+    changeSettings (newTransform) {
+      this.transform = newTransform
+    }
+  }
+}
 </script>
